46a390d519d7e9b50ba7ea28ae509a6f5d760bb6
Joyanta Mondal <56169323+w3codelabs@users.noreply.github.com>
2021-10-03 17:46:01 +0600

    jj

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h2>404!</h2> <br />
+            <p>Page Not Found</p>
+        </div>
+    );
+};
+
+export default NotFound;
